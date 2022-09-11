527d2ed9d8ceb98f9df5a1d48b695bc6e3e67b8a
Vizemo <vzerim14games+github@gmail.com>
2022-09-10 19:26:07 -0500

    fixed the decompression PY program

--- preProcessing/streaming_decompression.py
+++ preProcessing/streaming_decompression.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import json
 import zstandard
 from zstandard import ZstdDecompressor
@@ -35,42 +34,4 @@ with open("RC_2020-03.zst", 'rb') as fh:
                 #Write our JSON to the output file
                 f.write(json_data)
 
-=======
-import json
-import zstandard
-from zstandard import ZstdDecompressor
-
-with open("RC_2020-03.zst", 'rb') as fh:
-    dctx = zstandard.ZstdDecompressor(max_window_size=2147483648)
-    f = open("sub_count_input.txt", 'w', encoding='utf-8')
-    data = {}
-
-    with dctx.stream_reader(fh) as reader:
-        previous_line = ""
-        
-        while True:
-            chunk = reader.read(2**24)  # 16mb chunks
-            if not chunk:
-                break
-
-            string_data = chunk.decode('utf-8')
-            lines = string_data.split("\n")
-
-            for i, line in enumerate(lines[:-1]):
-                if i == 0:
-                   line = previous_line + line
-                object = json.loads(line)
-
-                #Store the relevant information in our JSON
-                data['comment'] = object['body']
-                data['score'] = object['score']
-                data['subreddit'] = object['subreddit']
-                
-                #Load our JSON data in a writable format
-                json_data = json.dumps(data)
-
-                #Write our JSON to the output file
-                f.write(json_data)
-
->>>>>>> 1bc4822695e683cbe2814bdd03ac7bb0f3b0d764
             previous_line = lines[-1]
