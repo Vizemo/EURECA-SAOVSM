6542f821f2572c1d19e790c17b684a7cf7fad01b
Mika Morgan <112218152+mika-morgan@users.noreply.github.com>
2022-09-16 16:33:58 -0500

    Update count_comments.py

--- preProcessing/count_comments.py
+++ preProcessing/count_comments.py
@@ -5,33 +5,14 @@ of = open("2020-01-comment-count.txt", 'w', encoding='utf-8') # opens outfile
 count = 0
 monthYear = '01 2020'
 
-#with open("D:\\EURECA\\processed\\2020-02.txt", 'r', encoding='utf-8') as f:
-#    for line in f:
-#        data = json.loads(line)
-#        print(data)
-
 #with open("D:\\EURECA\\processed\\2020-01.txt", 'r', encoding='utf-8') as f:
-#    lines = f.read().splitlines()
-#    print(lines)
-#    df_inter = pd.DataFrame(lines)
-#    count += 1
-#    df_inter.columns = ['comment']
 
 #open json file
-with pd.read_json("D:\\EURECA\\processed\\2020-02.txt", lines=True, chunksize=1) as reader:
+with pd.read_json("test_json.txt", lines=True, chunksize= 10) as reader:
     reader
+    print("opened file")
     for chunk in reader:
-        print(chunk)
-#print(count)
+        print(chunk)   
+        break
     
-    
-    
-#    count += 1
-
-
-#find the comment section count and go on to the next one
-
-
-
-
 #f.write(f'{monthYear} had {count} comments.')
