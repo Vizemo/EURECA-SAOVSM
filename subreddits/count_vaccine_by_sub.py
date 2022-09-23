e103e65e2ea250e9f1d800785ff0e432a4f2a7d0
Mika Morgan <112218152+mika-morgan@users.noreply.github.com>
2022-09-23 15:19:29 -0500

    Update count_vaccine_by_sub.py

--- subreddits/count_vaccine_by_sub.py
+++ subreddits/count_vaccine_by_sub.py
@@ -3,8 +3,14 @@ total_vaccine_count = []
 #Open the input file and read through each subreddit
 with open('all_subreddits.txt','r', encoding='utf-8') as f:
 
-    for line in f:
-        temp = tuple(line, 0)
-        total_vaccine_count.append(temp)
+  for line in f:
+    #Chop off the last character, to remove the new line character
+    line2 = line[:-1]
+    
+    #Pair each subreddit name with a counter
+    temp = (line2, 0)
+
+    #Add each pairing to the list
+    total_vaccine_count.append(temp)
 
 print(*total_vaccine_count, sep = "\n")
