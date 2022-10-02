6cc3ca076786bc4ec9709f69954ae44f501bdf9e
Mika Morgan <112218152+mika-morgan@users.noreply.github.com>
2022-10-02 17:27:50 -0500

    Update count_vaccine_by_sub.py

--- subreddits/count_vaccine_by_sub.py
+++ subreddits/count_vaccine_by_sub.py
@@ -1,16 +1,45 @@
-total_vaccine_count = []
+import pandas as pd
 
-#Open the input file and read through each subreddit
-with open('all_subreddits.txt','r', encoding='utf-8') as f:
+#Create the output file to write the subreddit frequency counts to, in order
+f1 = open("vaccine_frequency_by_sub_01.txt", 'w', encoding='utf-8')
 
-  for line in f:
-    #Chop off the last character, to remove the new line character
-    line2 = line[:-1]
-    
-    #Pair each subreddit name with a counter
-    temp = (line2, 0)
+total_vaccine_count = {}
+sub_list = []
 
-    #Add each pairing to the list
-    total_vaccine_count.append(temp)
+#Open the vaccination file and read through each subreddit
+print("Started reading JSON input file")
+with pd.read_json("vaccine_output_01.txt", lines = True, chunksize = 1000) as f:
 
-print(*total_vaccine_count, sep = "\n")
+    for chunk in f:
+
+        #Iterate through each row in 1,000 row chunks
+        for row in chunk.itertuples():
+
+            #Add each subreddit to the list
+            sub_list.append(row.subreddit)
+
+sl_unique = [*set(sub_list)]
+
+#Add each item in the sub_list to the counter dictionary
+total_vaccine_count = dict.fromkeys(sl_unique, 0)
+
+#Open the vaccination file and read through each comment
+print("Started reading JSON input file")
+with pd.read_json("vaccine_output_01.txt", lines = True, chunksize = 1000) as f:
+
+    for chunk in f:
+
+        #Iterate through each row in 1,000 row chunks
+        for row in chunk.itertuples():
+
+            #If the subreddit is found in all subreddit list, increment the counter
+            if row.subreddit in total_vaccine_count:
+               total_vaccine_count[row.subreddit] += 1
+            else:
+                print(row.subreddit)
+
+#Write the results to output file
+for w in sorted(total_vaccine_count, key=total_vaccine_count.get, reverse=True):
+    line = str(w) +': ' + str(total_vaccine_count[w])
+    f1.write(line)
+    f1.write('\n')
